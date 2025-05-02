5ce0cf038894e73674ef8302cedfbeeca907896b
ItsSunnyMonster <100400733+ItsSunnyMonster@users.noreply.github.com>
2025-05-02 18:25:45 +1200

    add timezone customisation

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # Home Page
 
-More info later.
+## How to display other timezones:
+
+Visit the URL with this format:
+
+```
+https://home.loooo.lol/?tz=[insert timezones here]
+```
+
+The TZ parameter should be formatted like this: Name:Timezone,Name2:Timezone2.
+
+A list of all valid timezones can be found [here](https://en.wikipedia.org/wiki/List_of_tz_database_time_zones) (Look for the TZ identifier section).
--- src/components/GlobalTimes.tsx
+++ src/components/GlobalTimes.tsx
@@ -1,14 +1,29 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
 import CurrentTime from "./CurrentTime";
 import Timezone from "./Timezone";
 
 export default function GlobalTimes() {
+  const params = useSearchParams();
+
+  const tz = params.get("tz")?.split(",");
+
   return (
     <div className="text-ctp-text flex flex-col items-center gap-3">
       <CurrentTime />
       <div className="flex gap-10">
-        <Timezone name="Auckland" timezone="Pacific/Auckland" />{" "}
-        <Timezone name="Fuzhou" timezone="Asia/Shanghai" />{" "}
-        <Timezone name="Brisbane" timezone="Australia/Brisbane" />{" "}
+        {tz ? (
+          tz?.map((tz) => {
+            const comps = tz.split(":");
+
+            const zone = comps[1].replaceAll("\\", "/");
+
+            return <Timezone name={comps[0]} timezone={zone} key={comps[0]} />;
+          })
+        ) : (
+          <></>
+        )}
       </div>
     </div>
   );
