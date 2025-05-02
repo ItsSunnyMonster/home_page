0e9607cd17d9a0e504f1760247001ca79dd485d1
ItsSunnyMonster <100400733+ItsSunnyMonster@users.noreply.github.com>
2025-05-02 18:28:20 +1200

    add suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import GlobalTimes from "@/components/GlobalTimes";
 import TimeOfDay from "@/components/TimeOfDay";
 import WeatherWidget from "@/components/WeatherWidget";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <>
-      <GlobalTimes />
+      <Suspense>
+        <GlobalTimes />
+      </Suspense>
 
       <p className="font-bold text-ctp-text text-[8cqw] lg:text-8xl xl:text-9xl">
         ~ Good <TimeOfDay /> ~
